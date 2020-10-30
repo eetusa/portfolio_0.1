0e99419d4c0c339244107f202293521f6952efcd
Eetu Salli <eetu.salli@student.samk.fi>
2020-10-31 01:38:14 +0200

    some content tweaks

--- src/About.js
+++ src/About.js
@@ -20,7 +20,7 @@ const About = () => {
 
         hobbies = () => {
             return(<div>
-            <small>I've been around computers near-literally my whole life, having various hobbies and interests involving them. I've always been an avid PC gamer, going through fpses, morpgs and mobas. I've also played guitar for the better part of my life. Movies and series definitely also list in my interests.</small></div>
+            <small>I've been around computers near-literally my whole life, having various hobbies and interests involving them. I've always been an avid PC gamer, going through fpses, morpgs and mobas. I've also played guitar for the better part of my life. Movies and series definitely also list in my interests. Horror movies in particular do rock my boat.</small></div>
             )
         }
         age="Age: ";
@@ -31,9 +31,12 @@ const About = () => {
         experience = () => {
             return(
                 <p className="lead">
-                <b>My first touch with programming</b> was with CoolBasic as a kid, but had a long hiatus since. My return to the field was taking programming studies while studying to be a classroom teacher at Rauma, Finland. That is when I found myself loving it. After rekindling the passion I've studied Python, Java, JavaScript, PHP, HTML and CSS. Currently I'm learning React and Bootstrap, both of which are used in this site. I started studies with programming as my major in 2020.<br></br><br></br>
+                <b>My first touch with programming</b> was with CoolBasic as a kid, but had a long hiatus since. My return to the field was when I took programming studies while studying to be a classroom teacher at Rauma, Finland. That is when I found myself loving the problem solving, the never-ending learning. After rekindling the passion I've studied Python, Java, JavaScript, PHP, HTML and CSS. Currently I'm learning React and Bootstrap. I started studies with programming as a major in 2020.<br></br><br></br>
                 
-                <b>My strengths</b> are self-learning, finding information, learning fast, applying learnt things in creative ways and always aiming for understanding rather than remembering. Most of my journey as a programmer has been self-learning or remote learning. I also have a strong background in mathematic which I think is an advantage.</p>
+                <b>My strengths</b>, I think, are self-learning, finding information, learning fast, applying learnt things in creative ways and always aiming for understanding rather than remembering. Most of my journey as a programmer has been self-learning or remote learning, so I feel pretty comfortable charging the unknown. I also have a strong background in mathematic which helps in certain tasks.<br></br><br></br>
+                
+                <b>My future is open</b> and I still don't know what I exactly want as a career a programmer. I'm looking forward to continuing to grow as coder and develope a more coherent view of the sub-categories in the field.
+                </p>
             )
         }
     }
