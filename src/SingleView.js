7c0169f95de130877217ba707378f595ff43fb00
Eetu Salli <eetu.salli@student.samk.fi>
2020-10-31 17:53:17 +0200

    snap

--- src/SingleView.js
+++ src/SingleView.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import data from './assets/data/projects.json';
 import useSite from './useSite';
+import useWindowDimensions from './useWindowDimensions';
+import Image from 'react-bootstrap/Image'
 
 
 const SingleView = () => {
     const {projectView} = useSite();
+    const {width} = useWindowDimensions();
     const lang = "EN";
     const index = projectView;
 
@@ -22,12 +25,20 @@ const SingleView = () => {
     }
 
     return (
-        <div className="container">
+        <div className="container py-4 px-0">
             <div className="row">
-                <div className="col-12">
-                    <div style={{overflow:"hidden"}}>
-                        <img src={require(`./assets/images/${pr.image}`)} alt=""></img>
-                    </div>
+                <div className="col-12 m-0 p-0" style={{position:"relative"}}>
+                        <Image fluid src={ width>=576 ?  require(`./assets/images/${pr.banner}`) : require(`./assets/images/${pr.image}`)  } alt=""></Image>
+                        <div 
+                            style={{
+                                width: "100%",
+                                padding: "10px",
+                                position: "absolute", 
+                                bottom:"0",
+                                background: "rgba(0,0,0,0.25)",
+                                color:"white"
+                        }}> {pr.title}</div>
+                        
                 </div>
             </div>
         </div>
