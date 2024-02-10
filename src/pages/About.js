f2f90889a127520e031f37237ad729d2fc1ea08d
Eetu Salli <eetu.salli@gmail.com>
2024-02-10 20:34:50 +0200

    Clean up about page, new picture of author

--- src/pages/About.js
+++ src/pages/About.js
@@ -38,13 +38,12 @@ const About = () => {
         
         return temp;
     }
-  //  console.log(loadTime)
 
     const interestsList = ["guitar", "music", "horror movies", "movies", "tv series", "piano", "games", "animals", "dogs", "mathemathics", 
-    "Dota 2", "CS:GO", "The Witcher 3", "tattoos", "code", "technology", "rain", "sea", "summer", "books", "audio books", "fantasy",
-    "scifi", "esports", "family", "friends", "learning", "studying", "teaching", "helping",  "beer", "wine", "sauna",
-     "hifi", "Pori","Rauma","Tampere","Kuopio", "js", "java", "full stack",
-    "sun","C++","C#","IoT", "Arduino", "cycling", "martin"];
+    "Dota 2", "CS2", "The Witcher 3", "tattoos", "code", "technology", "rain", "sea", "summer", "books", "audio books", "fantasy",
+    "scifi", "esports", "family", "friends", "learning", "studying", "teaching",  "beer", "wine", "sauna",
+     "hifi", "Pori", "Sastamala","Tampere", "js", "java", "full stack", "cyber punk",
+    "sun","C++","C#","IoT", "cycling", "martin guitars", "backend"];
 
     
     const [interests] = useState(shuffle(shuffle(interestsList)));
@@ -72,11 +71,8 @@ const About = () => {
                             const positionXSecond = interestAttributes[index][1];
                             const randomDuration=interestAttributes[index][2];
                             const randomOpacity=interestAttributes[index][4];
-                           // const randomDelay=interestAttributes[index][3];
                        
                            const randomDelay=getCurrentAnimationTime(interestAttributes[index][5])
-                            // const randomDelay=1
-                            // console.log(randomDelay)
                             return(
                                 <React.Fragment key = {`${index}+${word}`}>
                                       
@@ -95,7 +91,6 @@ const About = () => {
                                                                 WebkitAnimationDelay: `${randomDelay}s`,
                                                                 OAnimationDelay: `${randomDelay}s`,
                                                                 left: `${positionXFirst}%`,
-                                                                //color: Math.random() > 0.9 ? "rgba(0,0,0,1)" : "rgba(0,0,0,0.1)",
                                                                 color : `rgba(0,0,0,${randomOpacity})`,
                                                             }  
                                                                 :
@@ -110,7 +105,6 @@ const About = () => {
                                                                 WebkitAnimationDelay: `${randomDelay}s`,
                                                                 OAnimationDelay: `${randomDelay}s`,
                                                                 right: `${positionXSecond}%`,
-                                                               // color: Math.random() > 0.9 ? "rgba(0,0,0,1)" : "rgba(0,0,0,0.1)",
                                                                color : `rgba(0,0,0,${randomOpacity})`,
                                                             } 
                                                         }
@@ -131,19 +125,6 @@ const About = () => {
         
         <>
             <div className="row px-0 mx-0">
-                {/* <div className="col-12 p-4 m-0 order-0 order-sm-3"  style={{paddingTop:"0px", marginTop:"0px"}}>
-                    <RandomInterests />  
-                    <div 
-                        className="fade-in-second display-1"
-                        style={{
-                                position:"absolute",
-                                top:"25%",
-                                left:"30%"
-                            }}>
-                                Me
-                        </div>   
-                    
-                </div> */}
                 <div className="col-12 col-sm-5 col-lg-4 m-0 p-0 fade-in-first order-2 order-sm-3" 
                     style=
                         {{
@@ -164,7 +145,7 @@ const About = () => {
                                  maxWidth:"100%"
                                   
                             }}  
-                        src={require(`../assets/images/siteimages/minakuva2.png`)} 
+                        src={require(`../assets/images/siteimages/minakuva4.png`)} 
                         alt="Me" 
                     />
                 </div>       
@@ -208,7 +189,8 @@ const About = () => {
                 <p>I'm..</p>
                 <ul style={{paddingTop:"0px"}}>
                     <li><AgeFromDate birthdate={"1991-08-13"}/> years old</li>
-                    <li>interested in technology, <span data-for="music" data-type="success" className="text-success" data-tip="">music</span>, movies, <span data-for="games" data-type="error" className="text-danger" data-tip="">games</span>, tv series, family & friends</li>
+                    <li>interested in technology, <span data-for="music" data-type="success" className="text-success" data-tip="">music</span>, movies, 
+                    <span data-for="games" data-type="error" className="text-danger" data-tip=""> games</span>, tv series, family & friends</li>
                     <li>a happy owner of a Portuguese Water Dog, <span data-for='kelmi' className="text-info" data-tip='Kelmi'>Kelmi</span></li>
                 </ul>
                 <p>Prior to software development I studied to be a classroom teacher and a bit of mathemathics.</p>
@@ -224,9 +206,7 @@ const About = () => {
                     <li><span className="highlight-word">A longshoreman</span> in Port of Rauma <i>Valdoring Oy</i></li>
                     <li><span className="highlight-word">A warehouse worker</span> in Rauma shipyard <i>Transval Oy</i></li>
                 </ul>
-                {/* <p>My current abilities in computing:</p>
-                <div className="px-4 py-2" style={{backgroundColor:"rgb(230,230,230)"}}><p style={{paddingBottom:"2px", paddingTop:"2px", marginTop:"2px",marginBottom:"2px"}}><b>I have ability</b> in JS, Java, Python, C# and C/C++. In web development I'm familiar with HTML, CSS, PHP and React. This site has been built with React and uses Bootstrap.</p> <p style={{paddingBottom:"2px", paddingTop:"2px", marginTop:"2px",marginBottom:"2px"}}><b>Currently</b> I'm getting familiar with mobile device app development, VR applications and data analytics.</p></div> */}
-                <ReactTooltip id="games" className="tooltip-text" effect="solid">I'm a pretty good shot in CS:GO and quite alright in Dota 2!</ReactTooltip>
+                <ReactTooltip id="games" className="tooltip-text" effect="solid">I'm a pretty good shot in FPS games and quite alright in Dota 2!</ReactTooltip>
                 <ReactTooltip id='kelmi'  className="tooltip-kelmi" place="right"  effect="solid"><img alt="Kelmi dog" style={{position:"absolute",maxHeight:"100%", top:"0",left:"0"}} src={require(`../assets/images/siteimages/kelmi.png`)}/></ReactTooltip>
                 <ReactTooltip id="music" className="tooltip-text" effect="solid">I play guitar! And love to play other instruments too.</ReactTooltip>
         </ReadMore>
