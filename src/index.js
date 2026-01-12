8b2eb5319816e8cc131a0a5448ca5f93bf55340f
Eetu Salli <eetu.salli@gmail.com>
2026-01-12 21:19:15 +0200

    new project, centered fullscreen image carousel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import '@fortawesome/fontawesome-free/css/all.min.css'; 
-// import
-// 'bootstrap-css-only/css/bootstrap.min.css'; 
-// import
-// 'mdbreact/dist/css/mdb.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/data/style/app.css';
+import "react-image-gallery/styles/css/image-gallery.css";
 import App from './App';
 import { SiteProvider } from './api/site-context';
-
 
 ReactDOM.render(
   
